2e05e740e06e0149acb6d80fd7466c9f6a839940
SAINADH V <155620779+Frost730@users.noreply.github.com>
2025-01-06 13:36:34 +0530

    Update os_1.c

--- os_1.c
+++ os_1.c
@@ -15,3 +15,31 @@ else
 perror("\n fork faild\n");
 }
 
+// updated code for more info
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid > 0) {  // Parent process
+        wait(NULL); // Wait for the child process to finish
+        printf("completed\n");
+    } 
+    else if (pid == 0) {  // Child process
+        execl("/bin/ls", "ls", "-s", NULL);
+        perror("\nexecl failed\n");
+        exit(EXIT_FAILURE); // Exit child process if execl fails
+    } 
+    else {  // Fork failed
+        perror("\nfork failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0; // Indicate successful completion of the program
+}
+
+
